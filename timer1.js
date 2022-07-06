b20edf0bd9ea38169b7ad222a58a172bd0d0bbf8
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-06 01:28:16 +0000

    Added comments

--- timer1.js
+++ timer1.js
@@ -1,6 +1,7 @@
 const input = process.argv.slice(2);
 
 for(const timer of input) {
+  // ignore any negative numbers or non-numbers
   if (timer > 0)
     setTimeout(() => {
       process.stdout.write('BEEP ' + '\n');
